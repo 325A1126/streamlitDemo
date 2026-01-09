17aa37798b78e7c48c662fbf41ac3e53c8955cb6
325A1126 <st325a1126t.yamada@urayama.ac.jp>
2026-01-09 11:14:53 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ if "ans" not in st.session_state:
 if "cnt3" not in st.session_state:
     st.session_state.cnt3 = 0
 
+#履歴
+if "history" not in st.session_state:
+    st.session_state.history = []
+    
 #タイトル
 st.title("数当てゲーム(アプリ版)")
 
@@ -27,6 +31,7 @@ reset = st.button("リセット")
 if reset:
     st.session_state.ans = [randint(0, 9) for _ in range(3)]
     st.session_state.cnt3 = 0
+    st.session_state.history = []
     
 #判定
 elif judge:
@@ -34,6 +39,7 @@ elif judge:
     cnt1 = 0
     cnt2 = 0
     st.session_state.cnt3 += 1
+    
     for n in range(3):
         if ans[n] == nums[n]:
             cnt1 += 1
@@ -44,7 +50,17 @@ elif judge:
         st.write("正解です")
         st.image("omedetou.png", width= 200)
     else:
+        st.session_state.history.append(f"{st.session_state.cnt3}回目：{nums}　→　{cnt1}ヒット{cnt2}ボール")
         st.write(f"{cnt1}  ヒット  {cnt2}  ボール")
+
 
 #挑戦回数の表示       
 st.write(f'挑戦回数：{st.session_state.cnt3}回')
+
+# 履歴表示
+st.subheader("判定履歴")
+if st.session_state.history:
+    for h in st.session_state.history:
+        st.write(h)
+else:
+    st.write("まだ履歴はありません")
